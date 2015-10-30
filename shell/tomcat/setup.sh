e62805e5448fe8cf00054f95cc934dbcf0cb38cd
rcjamessr <rcjamessr@gmail.com>
2015-10-30 12:23:53 -0400

    Update setup.sh

--- shell/tomcat/setup.sh
+++ shell/tomcat/setup.sh
@@ -1,3 +1,3 @@
 cd /etc/chef/src/cookbooks
-wget -O tomcat 'https://supermarket.chef.io/cookbooks/tomcat7/download'
-tar -xvzf tomcat
+wget -O tomcat7 'https://supermarket.chef.io/cookbooks/tomcat7/download'
+tar -xvzf tomcat7
