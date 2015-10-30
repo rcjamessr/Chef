d67399172521c570aac7c50f3e3d1ec12462d094
rcjamessr <rcjamessr@gmail.com>
2015-10-30 08:39:23 -0400

    Update chefdeploy.sh

--- shell/chefdeploy.sh
+++ shell/chefdeploy.sh
@@ -1,4 +1,7 @@
+ sh /etc/chef/shell/java/setup.sh
+ sh /etc/chef/shell/tomcat/setup.sh
+ 
  chef-solo -o 'recipe[common]'
-  chef-solo -o 'recipe[java]'
+ chef-solo -o 'recipe[java]'
  chef-solo -o 'recipe[tomcat]'
  chef-solo -o 'recipe[HelloWorldJSP]'
