30bd9672cd5e51b07703d26e85a047633eafe619
rcjamessr <rcjamessr@gmail.com>
2015-10-29 20:32:55 -0400

    Update chefsetup.sh

--- shell/chefsetup.sh
+++ shell/chefsetup.sh
@@ -1,5 +1,5 @@
 apt-get update
 apt-get install curl
- curl -L https://www.opscode.com/chef/install.sh | sudo bash
- mkdir /etc/chef
+curl -L https://www.opscode.com/chef/install.sh | sudo bash
+mkdir /etc/chef
  
